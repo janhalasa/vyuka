05ced987412f0312675f221850f973f848f136f0
Ján Halaša <jan@halasa.com>
2022-10-24 19:39:57 +0200

    Update SumOfNumbers.java

--- java/SumOfNumbers.java
+++ java/SumOfNumbers.java
@@ -10,9 +10,17 @@ import java.util.List;
 public class SumOfNumbers {
 
     public static void main(String[] args) throws IOException {
+        
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         List<Integer> numbers = new LinkedList<>();
+        
+        // Do tejto premennej si budeme postupne ukladať hodnotu súčtu všetkých čísel
         int sum = 0;
+        
+        // For cyklus sa nastavuje troma časťami:
+        // 1. inicializácia "int i = 1" vytvorí premennú i, ktorá riadi cyklus
+        // 2. podmienka "i <= 5" určuje, či sa cyklus vykoná (ak platí) alebo sa preskočí (ak neplatí)
+        // 3. krok na konci cyklu "i++" zvýši hodnotu premennej i o 1
         for (int i = 1; i <= 5; i++) {
             System.out.println("Enter number " + i + ":");
             Integer number = Integer.parseInt(reader.readLine(), 10);
