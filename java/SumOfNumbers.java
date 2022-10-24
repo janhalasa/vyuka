8c01353aee1d31420c6af2e7d8e445caa2a1fd34
Ján Halaša <jan@halasa.com>
2022-10-24 19:30:58 +0200

    Create SumOfNumbers.java

---
+++ java/SumOfNumbers.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> numbers = new LinkedList<>();
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Enter number " + i + ":");
+            Integer number = Integer.parseInt(reader.readLine(), 10);
+            numbers.add(number);
+            sum = sum + number;
+        }
+        System.out.println("Sum of " + numbers + " = " + sum);
+    }
+}
