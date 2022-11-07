bb32a72bec1f267dac844481af1ef9b029ad905d
Ján Halaša <jan@halasa.com>
2022-11-07 13:08:51 +0100

    Improve variable naming

--- java/BubbleSort.java
+++ java/BubbleSort.java
@@ -20,9 +20,9 @@ public class BubbleSort {
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = 0; j < numbers.length - 1 - i; j++) {
                 if (numbers[j] > numbers[j + 1]) {
-                    int tempNumber = numbers[j + 1];
+                    int smallerNumber = numbers[j + 1];
                     numbers[j + 1] = numbers[j];
-                    numbers[j] = tempNumber;
+                    numbers[j] = smallerNumber;
                 }
             }
         }
